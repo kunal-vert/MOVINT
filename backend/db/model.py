c1d42fadd75725a5cc5e7557a13bc569e4e6a787
kunal <kunalk8728@gmail.com>
2026-08-30 21:29:04 +0530

    created the checkpoint model

--- backend/db/model.py
+++ backend/db/model.py
@@ -46,6 +46,65 @@ class Traveler(Base):
 
     Entry_time: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow, server_default=func.now())
 
+    #relationship
+
+
+
+
+class Checkpoint(Base):
+    __tablename__ = "checkpoint"
+
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4 
+    )  
+
+    name: Mapped[str] = mapped_column(
+        String(100), nullable=False
+    )  
+
+    post_Checking: Mapped[str] = mapped_column(
+        kunalEnum(CheckpointType), nullable=False, default=kunalEnum(CheckpointType)
+    )
+
+    state: Mapped[str] = mapped_column(
+        String(50), nullable=False
+    )
+
+    district: Mapped[Optional[str]] = mapped_column(
+        String(300), nullable=True
+    )
+
+    is_entry_point: Mapped[bool] = mapped_column(
+        Boolean, default=False, server_default=text("false")
+    )
+    is_exit_point: Mapped[bool] = mapped_column(
+        Boolean, default=False, server_default=text("false")
+    )
+    is_active: Mapped[bool] = mapped_column(
+        Boolean, default=True, server_default=text("true")
+    )
+
+
+    #relationship
+
+
+
+
+
+
+# this will be highly confidential cause it will intergrated to central command and rn we ain't use it unless we won't have the ground testing
+
+class Officer(Base):
+        
+ 
+
+
+
+
+
+
+
 
     
 
