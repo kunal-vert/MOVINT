a836911e624dba24ea716fcb6aa6966d643283d1
kunal <kunalk8728@gmail.com>
2026-08-31 17:00:45 +0530

    insert the pydantic setting and used the .env

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,1 +1,12 @@
-secu
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    jwt_secret_key: str
+    jwt_algorithm: str = "HS256"
+    # jwt_expire_minutes: int = 60
+    database_url: str
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
