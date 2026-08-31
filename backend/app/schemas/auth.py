e6d3dce09fda40725d5ce9bcaf00818b45027dd5
kunal <kunalk8728@gmail.com>
2026-08-31 14:16:00 +0530

    had to romove the pattern way of validting the password

--- backend/app/schemas/auth.py
+++ backend/app/schemas/auth.py
@@ -1,23 +1,49 @@
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr, Field, field_validator
 
 
 class SignupRequest(BaseModel):
     username: str =  Field(
         min_length= 10,
-        examples=["StrongPassword123!"],
+        
         description=(
             "Username must be unique"
           )),
-    Email: EmailStr
+    
+    email: EmailStr
     password: str = Field(
-        min_length=20,
-        max_length=130,
-        pattern= r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).+$",
-        examples=["StrongPassword123!"],
-        description=(
-            "Password must contain at least one lowercase letter, "
-            "one uppercase letter, one number, and one special character"
-    ) )
+        min_length=8,
+        max_length=128,
+        examples=["StrongPassword123!"]
+    )
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, password: str) -> str:
+
+        if not any(char.islower() for char in password):
+            raise ValueError(
+                "Password must contain at least one lowercase letter"
+            )
+
+        if not any(char.isupper() for char in password):
+            raise ValueError(
+                "Password must contain at least one uppercase letter"
+            )
+
+        if not any(char.isdigit() for char in password):
+            raise ValueError(
+                "Password must contain at least one number"
+            )
+
+        if not any(not char.isalnum() for char in password):
+            raise ValueError(
+                "Password must contain at least one special character"
+            )
+
+        return password
+
+      
+    
 
 
 class SignInRequest(BaseModel):
