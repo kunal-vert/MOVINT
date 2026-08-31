5b15aecf2dfc30d417323ad6f2c792d2c11a17a5
kunal <kunalk8728@gmail.com>
2026-08-31 11:23:26 +0530

    created the admin model

--- backend/app/db/auth_model.py
+++ backend/app/db/auth_model.py
@@ -1,24 +1,38 @@
+import uuid
 
+from datetime import datetime
 from sqlalchemy.orm import Mapped, mapped_column, relationships
-from sqlalchemy import (String, int)
-from database import Base
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import String, DateTime
+from backend.app.db.database import Base
 
 
 class Admin (Base):
-    id: Mapped[str]  = mapped_column(
-        index=True, primary_key=True, nullable=False
+    __tablename__ = "admins"
+
+
+
+    id: Mapped[uuid.UUID]  = mapped_column(
+       UUID(as_uuid=True) , primary_key=True, default=uuid.uuid4
     )
 
     username: Mapped[str]  = mapped_column(
-        String(50), nullable=False
+        String(50), unique=True, nullable=False
     )
 
     email: Mapped[str]   = mapped_column(
-        String(50), nullable=False
+        String(50), unique=True, nullable=False
     )
 
-    password: Mapped[str]  = mapped_column(
-        String(100), nullable= False
+    hash_password: Mapped[str]  = mapped_column(
+        String(300), nullable= False
+    )
+
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.utcnow,
+        nullable=False,
     )
 
 
