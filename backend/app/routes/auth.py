030407abb444526ec44c805d28465563ee0093eb
kunal <kunalk8728@gmail.com>
2026-08-31 13:40:21 +0530

    finished the signup endpoint

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,5 +1,10 @@
-from fastapi import APIRouter
-from app.schemas.auth import SignupRequest
+from fastapi import APIRouter, HTTPException,Depends
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import Session
+from app.db.auth_model import Admin
+from app.db.database import get_db
+from app.schemas.auth import SignupRequest, SignInRequest
+from app.utils.admin_password import password_hasher
 
 router = APIRouter(
     prefix= "/Admin",
@@ -7,15 +12,49 @@ router = APIRouter(
 )
 
 @router.post("/signup")
-async def AdminSignup(data: SignupRequest):
+async def AdminSignup(
+    data: SignupRequest,
+    db: Session = Depends(get_db)
+    ):
 
-    return {
-        "message": " Admin Signup Successfull bruhh!!!"
-    }
+    try:
+        adminName = data.username
+        email = data.email
+        password = data.password
+
+        hashed_password = password_hasher(password)
+
+        new_admin = Admin(
+            adminName = adminName,
+            Email = email,
+            password = hashed_password,
+
+        )
+
+        db.add(new_admin)
+        db.commit()
+        db.refresh(new_admin)
+
+        return {
+            "message": "Admin Successful created",
+            "admin" : {
+                "username": new_admin.adminName,
+                "email": new_admin.email,
+                "password": new_admin.hash_password
+            }
+        }
+
+    except IntegrityError:
+        db.rollback()
+
+        raise HTTPException(
+            status_code=409,
+            detail= "Username or email already exists"
+        )   
 
 
 @router.post("/signin")
-async def AdminSignup(data):
+async def AdminSignin(data: SignInRequest):
 
 
     return {
