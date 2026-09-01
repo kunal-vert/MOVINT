740bbd31fe3a59f6b5852c0f84ac3ad2547fadb1
kunal <kunalk8728@gmail.com>
2026-09-01 14:34:57 +0530

    def the riskCalc as dynamic score

---
+++ backend/app/services/risk_cal.py
@@ -0,0 +1,5 @@
+from app.db.model import Journey, CheckpointEvent, Checkpoint, Traveler, Permit
+
+
+def RiskCalc(pastTrackdata: Journey, Currentdata: Traveler, whileGoingIssue: CheckpointEvent, CurrentLoc: Checkpoint ):
+    pass
