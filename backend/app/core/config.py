68dbb72c36856c0db565eced8975efc84d38b022
kunal <kunalk8728@gmail.com>
2026-09-01 12:04:58 +0530

    fix the url bugs in config.py and had to create the __init.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,12 +1,15 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    jwt_secret_key: str
-    jwt_algorithm: str = "HS256"
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str = "HS256"
     # jwt_expire_minutes: int = 60
-    database_url: str
+    POSTGRS_URL: str
 
-    class Config:
-        env_file = ".env"
+    
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8"
+    )
 
 settings = Settings()
