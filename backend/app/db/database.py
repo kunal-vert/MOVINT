8b63e3256005d1392b2fe5a787ffb405696a1a50
kunal <kunalk8728@gmail.com>
2026-09-01 12:06:22 +0530

    did mistake in importing from config settings for postgrs_url

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,11 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from app.core.config import settings
+
 
 # This wil be for  PostgreSQL — PostGIS ready (for future spatial queries)..... already we have written im postGIS for leaflet js
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:12345@localhost:5432/NE_MOVINT"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True, echo=True)
+
+engine = create_engine(settings.POSTGRS_URL , pool_pre_ping=True, echo=True)
 
 SessionLocal = sessionmaker(autoflush=False, autocommit = False, bind=engine)
 
