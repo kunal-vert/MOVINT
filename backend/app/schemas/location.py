336600f24310d26d7788db35bc74d9c79d529a29
kunal <kunalk8728@gmail.com>
2026-09-04 21:55:24 +0530

    initialized the schemas class lmfaoooo!

---
+++ backend/app/schemas/location.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, Field
+
+
+class PostDetailsTraveler(BaseModel):
+    pass
+
+
+
+
+class GetDetailsTraveler(BaseModel):
+    pass
+
+
+
+class PostCheckPostEvents(BaseModel):
+    pass
+
+
+
+
+class GetCheckPostEvents(BaseModel):
+    pass
+
+
+
