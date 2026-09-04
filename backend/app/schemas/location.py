2e125ba2cd49f90e22fda263d7cb757127dcd001
kunal <kunalk8728@gmail.com>
2026-09-04 22:04:42 +0530

    added few more schemas like get_past_details

--- backend/app/schemas/location.py
+++ backend/app/schemas/location.py
@@ -1,13 +1,29 @@
 from pydantic import BaseModel, Field
 
 
+
+
 class PostDetailsTraveler(BaseModel):
-    pass
+    passport_id: str = Field(
+        min_length=7,
+        max_length=50,
+
+        description= "passport_id must be valid dude!/ karen!"
+    )
+
+
+
 
 
 
 
 class GetDetailsTraveler(BaseModel):
+    pass
+
+
+
+
+class GetPastRecord(BaseModel):
     pass
 
 
