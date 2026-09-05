b737ee3715261184de252a636b210a9b83392111
kunal <kunalk8728@gmail.com>
2026-09-05 16:41:29 +0530

    fix some bugs such as nullable, missing relationship

--- backend/app/db/model.py
+++ backend/app/db/model.py
@@ -1,10 +1,10 @@
-from __future__ import __annotations__
+from __future__ import annotations
 from datetime import datetime, UTC
 from typing import List, Optional
 from enum import Enum as kunalEnum
 import uuid
 
-from sqlalchemy import( String, Boolean, Float, Index , ForeignKey,Text, text, Integer, DateTime, Date, JSON, func )
+from sqlalchemy import( String, Boolean, Float, Index , ForeignKey,Text, text, Integer, DateTime, Date, JSON, func, Enum )
 from sqlalchemy.dialects.postgresql import UUID, ARRAY,JSONB
 
 from sqlalchemy.orm import mapped_column, Mapped, relationship
@@ -34,11 +34,13 @@ class Traveler(Base):
 
     date_of_birth: Mapped[datetime] = mapped_column(DateTime, nullable=False)
 
-    gender: Mapped[str]  = mapped_column(String(15), nullable= True, default="unknown")
+    gender: Mapped[Optional[str]]  = mapped_column(String(15), nullable= True, default="unknown")
 
-    occupation: Mapped[Optional[str]] = mapped_column (String(50), nullable=False) 
+    occupation: Mapped[str] = mapped_column (String(50), nullable=False) 
+    # we need to be craefull => here we will update the existing occuption as traveler could be come as with different occuption for etc reason
 
-    visa_type: Mapped[str]  = mapped_column(str(100), nullable=False)
+
+    visa_type: Mapped[str]  = mapped_column(String(100), nullable=False)
 
     visa_number: Mapped[Optional[str]] = mapped_column(String(100), unique=True, nullable=True, default="xxxxx")
 
@@ -46,9 +48,9 @@ class Traveler(Base):
 
     watch_flag: Mapped[bool] = mapped_column(Boolean, default=False, server_default=text("false"))
 
-    criminal_record: Mapped[bool] = mapped_column(Boolean, nullable=True, default=False, server_default=text("false"))
+    criminal_record: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False, server_default=text("false"))
 
-    Entry_time: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow, server_default=func.now())
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow, server_default=func.now())
 
 
 
@@ -60,6 +62,10 @@ class Traveler(Base):
 
     permits: Mapped[List["Permit"]] = relationship(
         "Permit", back_populates="traveler"
+    )
+
+    alerts: Mapped[List["Alert"]] = mapped_column(
+        "Alert", back_populates="traveler"
     )
 
 
